e9b19b61fb1893de068a79206bfea01702bc84ba
Yuichiro Ueno <y1r.ueno@gmail.com>
2021-08-21 17:35:07 +0900

    disable log

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -231,7 +231,7 @@ func main() {
 	e := echo.New()
 	//e.Debug = true
 	//e.Logger.SetLevel(log.DEBUG)
-	e.Logger.SetLevel(log.WARN)
+	e.Logger.SetLevel(log.OFF)
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
